f006886aa20d07794c63cfc63a9092ca09e19126
violanerd <madalyne.cross@gmail.com>
2022-08-01 10:10:20 -0500

    added get office number to manager

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -10,21 +10,26 @@ test('creaates a manager object', () => {
 })
 
 test('gets manager name', () => {
-    const manager = new Manager('Ava', 2, 'me@me.com', 104);
+    const manager = new Manager('Ava', '2', 'me@me.com', '104');
     expect(manager.getName()).toEqual(expect.stringContaining('Ava'));
 })
 
 test('gets manager id', () => {
-    const manager = new Manager('Ava', 2, 'me@me.com', 104);
+    const manager = new Manager('Ava', '2', 'me@me.com', '104');
     expect(manager.getId()).toEqual(expect.stringContaining('2'));
 })
 
 test('gets manager email', () => {
-    const manager = new Manager('Ava', 2, 'me@me.com', 104);
+    const manager = new Manager('Ava', '2', 'me@me.com', '104');
     expect(manager.getEmail()).toEqual(expect.stringContaining('me@me.com'));
 })
 
 test('gets manager role', () => {
-    const manager = new Manager('Ava', 2, 'me@me.com', 104);
+    const manager = new Manager('Ava', '2', 'me@me.com', '104');
     expect(manager.getRole()).toEqual(expect.stringContaining('Manager'));
+})
+
+test('gets manager office number', () => {
+    const manager = new Manager('Ava', '2', 'me@me.com', '104');
+    expect(manager.getofficeNumber()).toEqual(expect.stringContaining('104'));
 })
